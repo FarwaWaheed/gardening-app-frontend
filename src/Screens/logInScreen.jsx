1c0d4e2b4f156ea7337f627ad47fc16c90de68ee
Farwa Waheed <farwawaheed714@gmail.com>
2025-04-09 11:57:14 +0500

    created login screen

---
+++ src/Screens/logInScreen.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import React from "react";
+import { FaGoogle, FaApple } from "react-icons/fa";
+import plantBg from '../assets/chris-lee-70l1tDAI6rM-unsplash1.png'; // Adjust the path
+
+export default function Login {
+    const [role, setRole] = useState("");
+
+    return (
+        <div className="flex w-screen h-screen bg-white">
+            <div className="w-1/2 bg-white flex flex-col justify-center items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl ">
+                {/*<div className="flex flex-col items-center m-md:items-start">*/}
+                <div className="w-full max-w-md space-y-2">
+                    <span className="text-3xl text-black font-serif font-bold mb-8">Get Started Now</span>
+                    <form className="w- max-w-md text-left">
+                        <div className="mt-8 mb-4">
+                            <label className="block text-medium text-black font-small mb-1">Name</label>
+                            <input
+                                type="text"
+                                placeholder="Enter your name"
+                                className="w-full text-black px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-green-500"
+                            />
+                        </div>
+
+                        <div className="mt-6 mb-4">
+                            <label className="block text-medium text-black mb-1">Email address</label>
+                            <input
+                                type="email"
+                                placeholder="Enter your email"
+                                className="w-full text-black px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-green-500"
+                            />
+                        </div>
+
+                        <div className="mt-6 mb-4">
+                            <label className="block text-medium text-black mb-1">Password</label>
+                            <input
+                                type="password"
+                                placeholder="Enter password"
+                                className="w-full text-black px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-green-500"
+                            />
+                        </div>
+
+                        <div className="mt-6 mb-4 relative">
+                            <label className="block text-medium text-black mb-1">Role</label>
+                            <select
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                                className="w-full text-black px-3 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-green-500"
+                            >
+                                <option value="">Select a role</option>
+                                <option value="gardener">Gardener</option>
+                                <option value="home-owner">Home Owner</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
+
+                        <div className="mt-8 mb-4 flex items-center">
+                            <input type="checkbox" className="mr-2 bg-white"/>
+                            <label className="text-sm text-black">
+                                I agree to the <span className="cursor-pointer">terms & policy</span>
+                            </label>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full bg-[#3A5B22] text-black py-2 rounded-md font-semibold hover:bg-green-800 transition"
+                        >
+                            Signup
+                        </button>
+                    </form>
+                </div>
+            </div>
+                {/* Image Section - 50% */}
+                <div className="w-1/2 h-full">
+                    <img className="w-2/3 h-full object-contain shadow-xl rounded-md" alt="Plant" src={plantBg}/>
+                </div>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Signup from "./Screens/signUpScreen.jsx";
+import Login from "./Screens/logInScreen.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
