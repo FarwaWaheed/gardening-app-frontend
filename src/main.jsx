80d6e29ecc099cfc5334c93580a2e365582603b3
umemaahmed009 <umemaahmed009@gmail.com>
2025-04-23 18:19:36 +0500

    screens folder is changed to lowercase

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Signup from "./Screens/signUpScreen.jsx";
-import Login from './Screens/logInScreen.jsx';
+import Signup from "./screens/signUpScreen.jsx";
+import Login from './screens/logInScreen.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
