eac160eeb9e206b259d4918263ed89725de77c0a
umemaahmed009 <umemaahmed009@gmail.com>
2025-04-30 20:57:30 +0500

    working form created

--- src/Pages/AddPlantNote.jsx
+++ src/Pages/AddPlantNote.jsx
@@ -2,14 +2,11 @@ import React, { useState } from 'react';
 import { useNavigate, useParams} from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import validatePlantForm from '../utils/validatePlantForm';
 import { addPlantRecord } from '../api/plantRecordsApis.js';
 import BackButton from '../components/BackButton';
 import FormTextInput from "../components/FormTextInput.jsx";
-
-
 export default function AddPlantNote() {
-    const id = useParams();
+    const { id} = useParams();
     const userId = localStorage.getItem("userId");
     const [errors, setErrors] = useState({});
     const [loading, setLoading] = useState(false);
@@ -39,20 +36,18 @@ export default function AddPlantNote() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!validatePlantForm(formData, setErrors)) return;
-
+        // if (!validatePlantForm(formData, setErrors)) return;
         setLoading(true);
-
         try {
-            const response = await addPlantRecord(userId, id,formData);
-            console.log('Plant record saved:', response.record);
+            const response = await addPlantRecord(userId, id,formData.image, formData.height, formData.notes);
+            console.log('Plant record saved:', response);
             alert('Plant record added successfully!');
-
+            navigate(`/home/${id}/allNotes`);
         } catch (error) {
             console.error('Error saving plant record:', error);
-            alert('Something went wrong while saving the plant record.');
         } finally {
             setLoading(false);
+
         }
     };
 
@@ -65,9 +60,9 @@ export default function AddPlantNote() {
                 <BackButton/>
                 <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow space-y-4">
 
-                    <FormTextInput label="Height" name="height" value={formData.height} onChange={handleChange}
+                    <FormTextInput label="Height (in cm)" name="height" value={formData.height} onChange={handleChange}
                                    error={errors.height}/>
-                    <FormTextInput label="Observations" name="observations" value={formData.notes} onChange={handleChange}
+                    <FormTextInput label="Observations" name="notes" value={formData.notes} onChange={handleChange}
                                    error={errors.notes}/>
                     <div className="mb-4">
                         <label className="block mb-1 text-sm font-medium text-gray-700">Upload Image</label>
@@ -81,18 +76,15 @@ export default function AddPlantNote() {
                         {errors.image && <p className="text-red-500 text-sm mt-1">{errors.image}</p>}
                     </div>
                     {/*<FormTextInput label="Image URL" name="imageUrl" value={formData.imageUrl} onChange={handleChange} error={errors.imageUrl} />*/}
-
                     <button
                         type="submit"
                         disabled={loading}
                         className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition disabled:opacity-50"
                     >
+                        Submit
                     </button>
                 </form>
-                );
             </main>
-
-
             <Footer />
         </div>
     );
