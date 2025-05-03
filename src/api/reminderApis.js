5b9dda2709340ace91e90435ace4dffc82f2dcd4
umemaahmed009 <umemaahmed009@gmail.com>
2025-05-03 23:34:20 +0500

    reminder apis created

---
+++ src/api/reminderApis.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:5000/user"
+
+export const addReminder = async (userId, plantId, taskType, date, notes) => {
+    try {
+        const response = await axios.post(
+            `${BASE_URL}/addReminder/${userId}/${plantId}`,
+            {
+                "taskType" :taskType,
+                "date": date,
+                "notes":notes,
+            },
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error;
+    }
+};
+
+export const getPlantRecord = async (userId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/getReminders/${userId}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error;
+    }
+}
+
+export const updateReminder = async (reminderId, data) => {
+    try {
+        const response = await axios.put(
+            `${BASE_URL}/updateReminder/${reminderId}`,
+            {data},
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error;
+    }
+};
+
+
+export const deletePlantRecord = async (reminderId) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/deleteReminder/${reminderId}`);
+        return response.message;
+    } catch (error) {
+        throw error.response?.message || error;
+    }
+}
